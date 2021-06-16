a8791acf4d11fdbc021367f3c02bf07a936ca65a
skrahat <asaduzzaman.rahat@mail.mcgill.ca>
2021-06-16 23:53:49 +0600

    updating

--- react-backend/app.js
+++ react-backend/app.js
@@ -20,6 +20,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//allow cross-origin resource sharing
+app.use(function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers","Origin , X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/reward', rewardRouter);
